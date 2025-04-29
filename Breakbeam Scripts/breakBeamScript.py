4f07b289d969577cf7ac8d629a6c866c405fd063
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2025-04-29 11:57:30 -0400

    Server Update

--- Breakbeam Scripts/breakBeamScript.py
+++ Breakbeam Scripts/breakBeamScript.py
@@ -1,4 +1,22 @@
 import RPi.GPIO as GPIO
+import socket
+import time
+import atexit
+
+SERVER_IP = "localHost"
+SERVER_PORT = 2222
+POLL_DELAY = .05
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+while True:
+    try:
+        socket.connect((SERVER_IP, SERVER_PORT))
+        print("Server Connected")
+        break
+    except socket.error:
+        print("Connection Failed: Retyring in 1 second")
+        time.sleep(1)
 
 pinList = ((18, "a"))
 GPIO.setmode(GPIO.BCM)
@@ -11,13 +29,18 @@ class breakBeamPin():
     def onBeamBreak(self):
         if GPIO.input(self.pin):
             print("Pin " + str(pin) + " broken")
+            message = str(self.pin)+"\n"
+            sock.sendall(message.encode())
+            time.sleep(POLL_DELAY)
         else:
             print("Pin " + str(pin) + " unbroken")
         
-
+def onExit():
+    sock.close()
+    GPIO.cleanup()
 
 
 for pin in pinList:
     breakBeamPin(pin[0], pin[1])
 
-
+atexit.register(onExit)
